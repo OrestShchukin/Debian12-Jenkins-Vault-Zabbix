3a65ddc22ec25ba0fbcc764b87c7fec26eea9a62
Orest Shchukin <orest.shchukin@gmail.com>
2026-04-23 20:47:05 +0300

    Minor fixes

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -7,12 +7,14 @@ chmod +x /vagrant/provision/*.sh
 
 /vagrant/provision/install_packages.sh
 /vagrant/provision/install_docker.sh
+/vagrant/provision/configure_docker_dns.sh
 /vagrant/provision/prepare_dirs.sh
 /vagrant/provision/generate_self_signed_certs.sh
 /vagrant/provision/create_systemd_unit.sh
 
-echo "[INFO] Starting Devops-stack service..."
-systemctl start devops-stack
+echo "[INFO] Starting Docker Compose services..."
+cd /opt/devops-stack/docker
+docker compose up -d --build
 
 /vagrant/provision/install_zabbix_agent.sh
 /vagrant/provision/configure_zabbix_agent.sh
